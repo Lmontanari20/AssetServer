15308ead50d141eb18f3bbb5aca4ff329ef0f01d
Lucas Montanari <lmontanari@csumb.edu>
2019-04-01 21:35:22 -0700

    fixed a path

--- AssetServer/src/main/java/Capstone/AssetServer/MainController.java
+++ AssetServer/src/main/java/Capstone/AssetServer/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller // this means the class is a controller
-@RequestMapping(path="/demo") // this is the path after the application path
+@RequestMapping(path="/api") // this is the path after the application path
 public class MainController {
     @Autowired // this gets the bean called assetRepository
     private AssetsRepository assetsRepository;
